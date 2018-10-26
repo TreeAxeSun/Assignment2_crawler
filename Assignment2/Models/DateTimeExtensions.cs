ea93d7c33912ea984c0c6bb8f6eb4a92ee941d2d
YoungSeok-Ahn <veris1975@outlook.com>
2018-10-26 11:09:10 -0500

    second commit

---
+++ Assignment2/Models/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public static class DateTimeExtensions
+    {
+        public static string CaculateAge(this int value)
+        {
+            return string.Format(string CaculateAge, value);
+        }
+    }
+}
---
+++ Assignment2/Models/HtmlExtentios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public static class HtmlExtentios
+    {
+        public static HtmalModal(this HtmlExtentios htmlExtentios)
+        {
+            var returnData = "";
+        }
+
+    }
+}
